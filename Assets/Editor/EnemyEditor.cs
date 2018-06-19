8b4a928a19ae71c8351879c578279b2813b1058c
Ryan Huffman <rch.huffman@gmail.com>
2018-06-18 20:20:46 -0700

    Initial commit consisting of the entire Unity project.

---
+++ Assets/Editor/EnemyEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemyScript1))]
+public class EnemyEditor : Editor {
+
+    //public override void OnInspectorGUI()
+    //{
+    //    EnemyScript1 myScript = (EnemyScript1)target;
+
+    //    myScript.missileBarrageParameters.usePercentage = GUILayout.Toggle(myScript.missileBarrageParameters.usePercentage, "Use Percentage");
+
+    //    foreach (var o in myScript.missileBarrageParameters.GetType().GetProperties())
+    //    {
+    //        if (o.GetType() == typeof(FloatParameters))
+    //        {
+    //            foreach(var i in o.GetType().GetProperties())
+    //            {
+    //                EditorGUILayout.FloatField((float)i., )
+    //            }
+    //        }
+    //    }
+
+    //    /*foreach(var o in myScript.missileBarrageParameters.GetType().GetProperties())
+    //    {
+    //        if(o.GetType() == typeof(FloatParameters))
+    //        {
+                
+    //        }
+    //    }
+    //    EditorGUI.BeginDisabledGroup(myScript.missileBarrageParameters.usePercentage);
+    //    EditorGUILayout.IntField("HELLO", 1);*/
+    //}
+}
